aff114d5da95f952da89bdf06252cb2e055c9c84
expLOCK <65228869+expLOCK@users.noreply.github.com>
2020-05-23 16:30:17 +0300

    Break added

--- disciplines.py
+++ disciplines.py
@@ -1,26 +1,27 @@
 from telebot import types
 
 disciplines_links = {
-    "AZAMAT": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/a.khotov"),
-    "introToSE": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/minaevosman"),
-    "history": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/hatmat73"),
-    "lifeSafety": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/yusupu"),
-    "english_subgroup_1": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/elinastar.ru"),
-    "english_subgroup_2": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/star1918"),
-    "mathAnalysis": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/aldymadina537"),
-    "probabilityTheory": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/zaya310387"),
-    "discreteMath": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/magomerzaev57"),
-    "cheHistory": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/nataev.s"),
-    "cheCultureAndEthics": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/sbeguev")
+    "AZAMAT": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/"),
+    "introToSE": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/"),
+    "history": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/"),
+    "lifeSafety": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/"),
+    "english_subgroup_1": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/"),
+    "english_subgroup_2": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/"),
+    "mathAnalysis": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/"),
+    "probabilityTheory": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/"),
+    "discreteMath": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/"),
+    "cheHistory": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/"),
+    "cheCultureAndEthics": types.InlineKeyboardButton(text='Подключиться', url="https://chesuru.webex.com/meet/"),
+    "break": types.InlineKeyboardButton(text="👍", callback_data="good")
 }
-disciplines_list = """[Программирование](https://chesuru.webex.com/meet/a.khotov)\n
-[Введение в ПИ](https://chesuru.webex.com/meet/minaevosman)\n
-[История](https://chesuru.webex.com/meet/hatmat73)\n
-[БЖД](https://chesuru.webex.com/meet/yusupu)\n
-[Математический Анализ](https://chesuru.webex.com/meet/aldymadina537)\n
-[Теория Вероятностей](https://chesuru.webex.com/meet/zaya310387)\n
-[Дискретная Математика](https://chesuru.webex.com/meet/magomerzaev57)\n
-[История ЧР](https://chesuru.webex.com/meet/nataev.s)\n
-[Чеченская Культура и Этика](https://chesuru.webex.com/meet/sbeguev)\n 
-[Английский 1 подгр](https://chesuru.webex.com/meet/elinastar.ru)\n 
-[Английский 2 подгр](https://chesuru.webex.com/meet/star1918)"""
+disciplines_list = """[Программирование](https://chesuru.webex.com/meet/)\n
+[Введение в ПИ](https://chesuru.webex.com/meet/)\n
+[История](https://chesuru.webex.com/meet/)\n
+[БЖД](https://chesuru.webex.com/meet/)\n
+[Математический Анализ](https://chesuru.webex.com/meet/)\n
+[Теория Вероятностей](https://chesuru.webex.com/meet/)\n
+[Дискретная Математика](https://chesuru.webex.com/meet/)\n
+[История ЧР](https://chesuru.webex.com/meet/)\n
+[Чеченская Культура и Этика](https://chesuru.webex.com/meet/)\n 
+[Английский 1 подгр](https://chesuru.webex.com/meet/)\n 
+[Английский 2 подгр](https://chesuru.webex.com/meet/)"""
